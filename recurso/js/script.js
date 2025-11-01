be859e0be7efca77a72dac07c9935cef7dbd3005
SALINAS TECH <salinastechbolivia@gmail.com>
2025-11-01 15:30:49 -0400

    script.js

--- recurso/js/script.js
+++ recurso/js/script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    console.log("Página de enlace de Salinas Tech cargada.");
 
-    document.querySelectorAll('.main-buttons a').forEach(button => {
-        button.addEventListener('click', (e) => {
-            const text = e.currentTarget.querySelector('span.left-icon').textContent.trim();
-        });
-    });
-});
